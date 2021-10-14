d4094eb3f0aa7bf6e63c4556373f7119c000d44b
Fatima Ayala <ftayala89@gmail.com>
2021-10-13 20:05:49 -0600

    Agregar archivos de proyecto.

---
+++ simpleCRUD/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace simpleCRUD
+{
+    class Cliente
+    {
+        //propiedades
+        public int _clienteId { get; set; }
+        public string _nombres { get; set; }
+        public string _address { get; set; }
+        public int _telephon { get; set; }
+        public int _mobile { get; set; }
+
+        //instancias a la clase Crud
+        private Crud crud = new Crud();
+
+        //metodo para retornar los registros de la tabla Book
+        public MySqlDataReader getAllBooks()
+        {
+            string query = "SELECT clienteId,nombres,address,telephon,mobile FROM cliente";
+
+            //llamado al metodo select de la clase Crud
+            return crud.select(query);
+        }
+    }
+}
